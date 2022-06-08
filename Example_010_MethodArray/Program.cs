6880812fd8f0457500ce6d22c249316d91377315
DAYNYUS <excaliburbmw@mail.ru>
2022-06-08 08:14:11 +0200

    add Example 010

---
+++ Example_010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 11, 21, 31, 41, 17, 15, 61, 17, 18, 19 };
+
+int n = array.Length;
+int find = 17;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if(array[index]
+        == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index = index + 1;
+}
