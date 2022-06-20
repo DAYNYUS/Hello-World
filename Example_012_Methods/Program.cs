ca5cf2c778d6a4bb58c0ff902e2f6dddf734adc1
unknown <excaliburbmw@mail.ru>
2022-06-20 10:47:57 +0200

    add Methods

---
+++ Example_012_Methods/Program.cs
@@ -0,0 +1,63 @@
+﻿// Method 1
+
+//void Method1()
+//{
+//    Console.WriteLine("Author ...");
+//}
+//Method1();
+
+
+
+// Method 2
+
+//void Method2(string msg)
+//{
+    //Console.WriteLine(msg);
+//}
+
+//Method2(msg: "Text of massage");
+
+//void Method21(string msg, int count)
+//{
+    //int i = 0;
+    //while (i < count)
+    //{
+        //Console.WriteLine(msg);
+        //i++;
+    //}
+    
+//}
+
+//Method21(msg:"Text", count: 4);
+//Method21(count: 4, msg:"New Text");
+
+
+// Method3
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+//Console.WriteLine(year);
+
+
+// Method4
+
+string Method4(int count, string text)
+
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "z");
+Console.WriteLine(res);
