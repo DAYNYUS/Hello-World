9697661b078dd431aa97f593044e66f6a17f2d6a
unknown <excaliburbmw@mail.ru>
2022-06-21 18:59:42 +0200

    add to example 014

--- Example_014_/Program.cs
+++ Example_014_/Program.cs
@@ -36,18 +36,33 @@ Console.WriteLine();
 }*/
 
 
-void PrintArray(int[] matr)
+void PrintArray(int[,] matr)
 {
     for (int i = 0; i < matr.GetLength(0); i++)
     {
         for (int j = 0; j < matr.GetLength(1); j++)
         {
-          Console.Write($"{matr[i,j]} ");
+            Console.Write($"{matr[i,j]} ");
         }
         Console.WriteLine();    
     }
 }
 
-int[,] matrix = new int[3,4];
+void FillArray(int[,] matr) //метод заполняет матрицу рандом
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10); //[1; 10)
+        }
+    }
+}
 
+
+int[,] matrix = new int[3,4];
 PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
